ce4ecfe2ba75823b48b375128a6d012e73cba048
Just-Whiz <christiansuy25@gmail.com>
2024-06-30 20:58:04 -0400

    Setting up Alpaca libraries and variables

---
+++ StockTradingBot1.0.py
@@ -0,0 +1,18 @@
+# Name: Christian Suy
+# Program name: Stock Trading Bot
+
+from lumibot.brokers import Alpaca
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.strategies.strategy import Strategy
+from lumibot.traders import Trader
+from datetime import datetime
+
+API_KEY = ""
+API_SECRET = ""
+BASE_URL = ""
+
+ALPACA_CREDS = {
+    "API_KEY": API_KEY,
+    "API_SECRET": API_SECRET,
+    "PAPER": True
+}
