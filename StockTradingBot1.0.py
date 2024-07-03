42dab7e0925d33afcb7367545c82fd139312c531
Just-Whiz <christiansuy25@gmail.com>
2024-07-02 20:26:03 -0400

    Added broker, strategy, and start end dates

--- StockTradingBot1.0.py
+++ StockTradingBot1.0.py
@@ -7,12 +7,35 @@ from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
 from datetime import datetime
 
-API_KEY = ""
-API_SECRET = ""
-BASE_URL = ""
+API_KEY = "PKRFBEWR0NTITQ3OIV19"
+API_SECRET = "l1L4x9KbY7hRsJgOLPt8deCk1qcqCW8zsUhgGkg5"
+BASE_URL = "https://paper-api.alpaca.markets"
 
 ALPACA_CREDS = {
     "API_KEY": API_KEY,
     "API_SECRET": API_SECRET,
     "PAPER": True
 }
+
+# Life cycle methods
+class MLTrader(Strategy):
+    # Runs once (everytime the bot starts up)
+    def initialize(self):
+        pass
+    # Runs on every tick (everytime new data is received)
+    def on_trading_iteration(self):
+        pass
+
+start_date = datetime(2024,1,15)
+end_date = datetime(2024,1)
+
+
+broker = Alpaca(ALPACA_CREDS)
+strategy = MLTrader(name="mlstrat", broker=broker, parameters={})
+
+strategy.backtest(
+    YahooDataBacktesting,
+    start_date,
+    end_date,
+    parameters={}
+)
